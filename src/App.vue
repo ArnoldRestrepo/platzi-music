5ed597ccb068600f0903b28e4ea723ad25985e76
Front-End Ondigital <ondigital.desarrollo@gmail.com>
2018-05-28 16:51:24 -0500

    Expresiones

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
-    Form
+    //img(src="./assets/logo.png")
+
+
+    //Directivas
+    p(v-show="showValue") {{ value }}
+
+    // Expresiones
+    // -
+      p {{ 1 + 1 }}
+      p {{ 'Hola' + 'Mundo' }}
+      p {{ person.name }}
+      p {{ person.name.toUpperCase() }}
+      p {{ JSON.stringify(person) }}
+      p {{ !true ? 'true' : 'false' }}
+    //
+
+
+    //Form
 </template>
 
 <script>
 import Form from './components/Form.vue'
 export default {
   name: 'app',
+  google: 'https://google.com',
+  googleName: 'Google Es',
   data () {
     return {
-      msg: 'Hola Mundo desde Vue.js'
+
+      /* Directives */
+      showValue: true
+
     }
   },
   components: {
