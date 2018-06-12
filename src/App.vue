4ae4a587387467e11c3579f1d659d0fd6ba7c8ae
Front-End Ondigital <ondigital.desarrollo@gmail.com>
2018-06-12 16:49:18 -0500

    Slots

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   #app
     pm-header
+
+    pm-notification(v-show="showNotification")
+      p(slot="body") No se encontraron resultados
     pm-loader(v-show="isLoading")
     section.section(v-show="!isLoading")
       nav.nav
@@ -32,12 +35,15 @@
 </template>
 
 <script>
+// Tracks Services
 import trackService from '@/services/track'
+import PmTrack from '@/components/Track'
+// Layout
 import PmFooter from '@/components/layout/Footer'
 import PmHeader from '@/components/layout/Header'
-
-import PmTrack from '@/components/Track'
+// Shared
 import PmLoader from '@/components/shared/Loader'
+import PmNotification from '@/components/shared/Notification'
 
 export default {
   name: 'app',
@@ -46,6 +52,7 @@ export default {
       searchQuery: '',
       tracks: [],
       isLoading: false,
+      showNotification: false,
       selectedTrack: ''
     }
   },
@@ -55,13 +62,21 @@ export default {
     }
   },
   watch: {
-
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
   },
   methods: {
     search () {
       if (!this.searchQuery) { return }
       this.isLoading = true
       trackService.search(this.searchQuery).then((res) => {
+        console.log(res)
+        this.showNotification = res.tracks.total === 0
         this.isLoading = false
         this.tracks = res.tracks.items
       })
@@ -74,13 +89,18 @@ export default {
     PmFooter,
     PmHeader,
     PmTrack,
-    PmLoader
+    PmLoader,
+    PmNotification
   }
 }
 </script>
 
 <style lang="scss">
 @import './scss/main.scss';
+
+body{
+  padding: 0;
+}
 
 .results{
   margin-top: 30px;
---
+++ src/components/shared/Notification.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .container
+    .columns
+      .column.is-5.is-offset-4
+        .notification.is-danger
+          slot(name="body") Algo anduvo mal
+</template>
+<style lang="scss" scoped>
+.notification {
+  margin: 1rem 0;
+}
+</style>
+
